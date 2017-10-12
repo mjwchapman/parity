fb72bbfb12ad23a990e27563ce16f5c060f6d0fe
Michael Chapman <michaelchapman1@yahoo.com>
2017-10-12 11:01:59 +0000

    Add java flight recorder run script

---
+++ applications/system/run-jfr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+java -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:FlightRecorderOptions=dumponexit=true,dumponexitpath=parity-system​.jfr,loglevel=info -XX:StartFlightRecording=defaultrecording=true,settings=profile -jar parity-system.jar etc/genesis.conf 2>&1 | tee parity-system.log
+
+
